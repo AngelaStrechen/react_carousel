3e3717df2e9781b286700c4ccf01e5841dd04db0
Angela <angelastrechen1@mail.com>
2026-03-01 12:03:18 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,56 @@ import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
-
-class App extends React.Component<{}, State> {
+class App extends React.Component {
   state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+    images: Array.from({ length: 10 }, (_, i) => `./img/${i + 1}.png`),
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
   };
 
   render() {
-    const { images } = this.state;
+    const { images, itemWidth, frameSize, step } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        {/* Контрол для ширини картинки */}
+        <label htmlFor="itemId">Item Width</label>
+        <input
+          id="itemId"
+          type="number"
+          value={itemWidth}
+          onChange={e => this.setState({ itemWidth: +e.target.value })}
+        />
+
+        {/* Контрол для frameSize */}
+        <label htmlFor="frameId">Frame Size</label>
+        <input
+          id="frameId"
+          type="number"
+          value={frameSize}
+          onChange={e => this.setState({ frameSize: +e.target.value })}
+        />
+
+        {/* Контрол для step */}
+        <label htmlFor="stepId">Step</label>
+        <input
+          id="stepId"
+          type="number"
+          value={step}
+          onChange={e => this.setState({ step: +e.target.value })}
+        />
+
+        {/* Сам карусель */}
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          data-cy="carousel"
+        />
       </div>
     );
   }
